ec1bfca3348c6c37e6ef4c54c8069f210307be71
Sebastian <sebastiandagostino@gmail.com>
2017-09-10 02:02:44 -0300

    Allow app to run with up to 6GB of RAM

--- gb.sh
+++ gb.sh
@@ -4,6 +4,6 @@ percentage=8
 
 base=100
 
-java -jar target/GraphBuilder-1.0-jar-with-dependencies.jar $1 $2 $3 $4 >file1.json
+java -Xmx4096m -Xms256m -jar target/GraphBuilder-1.0-jar-with-dependencies.jar $1 $2 $3 $4 >file1.json
 
-java -jar target/GraphBuilder-1.0-jar-with-dependencies.jar $1 $2 $3 $4 $(($1 * $percentage / $base)) >file2.json
+java -Xmx4096m -Xms256m -jar target/GraphBuilder-1.0-jar-with-dependencies.jar $1 $2 $3 $4 $(($1 * $percentage / $base)) >file2.json
